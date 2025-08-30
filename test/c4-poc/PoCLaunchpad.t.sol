61cd69df9ed53cbf839d3315d4c56e3be5ee6e4b
Alexander Papageorgiou <alex.ppg@protonmail.com>
2025-08-30 12:51:09 +0300

    Code: Update and rename PoC.t.sol to PoCLaunchpad.t.sol

--- test/c4-poc/PoCLaunchpad.t.sol
+++ test/c4-poc/PoCLaunchpad.t.sol
@@ -1,24 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {PoCTestBase} from "./PoCTestBase.t.sol";
+import {LaunchpadTestBase} from "./LaunchpadTestBase.sol";
 
-contract PoC is PoCTestBase {
+contract PoC is LaunchpadTestBase {
     /**
      * PoC can utilize the following variables to access the relevant contracts:
-     * ================LAUNCHPAD================
+     * - factory: ERC1967Factory.sol 
      * - launchpad: Launchpad.sol
      * - distributor: Distributor.sol
      * - curve: SimpleBondingCurve.sol
      * - launchpadLPVault: LaunchpadLPVault.sol
      * - quoteToken: Quote token used in Launchpad system
      * - uniV2Router: Uniswap V2 Router used in Launchpad system
-     * ================PERPETUAL================
-     * - factory: ERC1967Factory.sol 
-     * - perpManager: MockPerpManager.sol (extends PerpManager.sol)
-     * - gtl: GTL.sol 
-     * - usdc: Test USDC within perpetual system
-     * - ETH, GTE, BTC: Tickers for markets created
      */
     function test_submissionValidity() external {
     }
